b77db794ef3e74939f7f57983aedab3596d1de69
Vlad Kleshko <vkoesko@gmail.com>
2023-11-30 00:54:18 +0200

    Added CrewViewSSet to airport_service/views

--- airport_service/views.py
+++ airport_service/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from airport_service.models import Crew
+from airport_service.serializers import CrewSerializer
+
+
+class CrewViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.CreateModelMixin,
+                  GenericViewSet
+                  ):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
