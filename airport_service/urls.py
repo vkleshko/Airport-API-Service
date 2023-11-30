bf4403aa54644128e1d6ec18ae2146a641eb98c7
Vlad Kleshko <vkoesko@gmail.com>
2023-11-30 20:43:56 +0200

    Added 'routers/' path to airport_service/urls

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from airport_service.views import CrewViewSet, AirportViewSet
+from airport_service.views import (
+    CrewViewSet,
+    AirportViewSet,
+    RouteViewSet,
+)
 
 router = routers.DefaultRouter()
 router.register("crews", CrewViewSet)
 router.register("airports", AirportViewSet)
-
+router.register("routers", RouteViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
