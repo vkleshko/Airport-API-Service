d2ceed04d733246b84a65d19bcef941e826fe835
Vlad Kleshko <vkoesko@gmail.com>
2023-11-30 19:55:10 +0200

    Added AirportViewSet to airport_service/views

--- airport_service/views.py
+++ airport_service/views.py
@@ -1,14 +1,25 @@
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
-from airport_service.models import Crew
-from airport_service.serializers import CrewSerializer
+from airport_service.models import Crew, Airport
+from airport_service.serializers import CrewSerializer, AirportSerializer
 
 
-class CrewViewSet(mixins.ListModelMixin,
-                  mixins.RetrieveModelMixin,
-                  mixins.CreateModelMixin,
-                  GenericViewSet
-                  ):
+class CrewViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+
+
+class AirportViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
