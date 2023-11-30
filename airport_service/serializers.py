0319eae8aabfe16c59c65801187f0298bf7e4291
Vlad Kleshko <vkoesko@gmail.com>
2023-11-30 19:54:52 +0200

    Added AirportSerializer to airport_service/serializers

--- airport_service/serializers.py
+++ airport_service/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from airport_service.models import Crew
+from airport_service.models import Crew, Airport
 
 
 class CrewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Crew
         fields = ("id", "first_name", "last_name")
+
+
+class AirportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = ("id", "name", "closet_big_city")
