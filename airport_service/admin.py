b73f22e3bbdabc4f9475e924961d8ee99fb4f195
Vlad Kleshko <vkoesko@gmail.com>
2023-12-08 23:18:14 +0200

    Added TicketInline & OrderAdmin classes in airport_service/admin

--- airport_service/admin.py
+++ airport_service/admin.py
@@ -12,11 +12,21 @@ from airport_service.models import (
 
 )
 
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
 admin.site.register(Crew)
 admin.site.register(Airport)
 admin.site.register(Route)
 admin.site.register(AirplaneType)
 admin.site.register(Airplane)
 admin.site.register(Flight)
-admin.site.register(Order)
 admin.site.register(Ticket)
